a9785cae0fb860980b23b8248e1bd32827832e1a
Max Carvajal <jcarvajal288@gmail.com>
2025-10-03 19:38:53 -0500

    respond to changes in shooting direction

--- Entities/Characters/Components/StateMachine/States/Shoot/shoot.gd
+++ Entities/Characters/Components/StateMachine/States/Shoot/shoot.gd
@@ -5,6 +5,7 @@ extends State
 
 
 func process_frame(_delta: float) -> State:
+	animation_player.play_eight_way(animation_name, director.shooting_vector)
 	if director.shooting_vector == Vector2.ZERO:
 		if director.movement_vector == Vector2.ZERO:
 			return idle_state
