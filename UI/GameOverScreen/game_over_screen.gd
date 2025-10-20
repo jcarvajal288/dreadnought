ae702c9a7bd3d81960c345879b8e6e83805ebf91
Max Carvajal <jcarvajal288@gmail.com>
2025-10-19 21:28:40 -0500

    Reset spawner tracker on reset

--- UI/GameOverScreen/game_over_screen.gd
+++ UI/GameOverScreen/game_over_screen.gd
@@ -6,6 +6,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("reset"):
 		Global.is_game_over = false
+		Global.NUM_SPAWNERS = 0
 		get_tree().reload_current_scene()
 
 
