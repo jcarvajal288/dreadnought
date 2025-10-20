fd88f73c4c20c3fb07b37f369138064667a76d21
Max Carvajal <jcarvajal288@gmail.com>
2025-10-19 21:49:38 -0500

    Provisional victory screen

---
+++ UI/VictoryScreen/victory_screen.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+func _ready() -> void:
+	pass
+
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("reset"):
+		Global.is_game_over = false
+		Global.NUM_SPAWNERS = 0
+		get_tree().reload_current_scene()
+
+
+func set_score(final_score: int) -> void:
+	$CanvasLayer/ScoreLabel.text = "Final Score: %d" % final_score
--- main.gd
+++ main.gd
@@ -1,11 +1,23 @@
 extends Node
 
+@export var victory_screen_scene: PackedScene
 @export var game_over_screen_scene: PackedScene
 
 
 func _ready() -> void:
 	Global.game_over.connect(_on_game_over)
 	Global.add_enemy.connect(_on_add_enemy)
+
+
+func _process(_delta: float) -> void:
+	if Global.NUM_SPAWNERS <= 4:
+		declare_victory()
+
+
+func declare_victory() -> void:
+	Global.is_game_over = true
+	var victory_screen = victory_screen_scene.instantiate()
+	add_child(victory_screen)
 
 
 func _on_game_over() -> void:
